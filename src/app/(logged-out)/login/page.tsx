ba3f3b2ecdaef08cec5e388b2aa1494b90679beb
Jay Elemar Termulo <elemar.termulo@springboardph.com>
2024-06-12 15:56:41 +0800

    added login form

--- src/app/(logged-out)/login/page.tsx
+++ src/app/(logged-out)/login/page.tsx
@@ -1,15 +1,29 @@
 "use client"
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import LoginForm from "@/components/auth/login/LoginForm"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { PersonStandingIcon } from "lucide-react"
+import Link from "next/link"
 import React from "react"
 
 export default function LoginPage() {
   return (
     <>
-      <Card>
+      <PersonStandingIcon size={45} className="text-primary" />
+      <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle>Login</CardTitle>
           <CardDescription>Login to your support me account</CardDescription>
         </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+        <CardFooter className="flex justify-between items-center">
+          <small>Don&apos;t have an account?</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-ip"> Sign Up</Link>
+          </Button>
+        </CardFooter>
       </Card>
     </>
   )
